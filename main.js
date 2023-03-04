a338ed0e42241c5db4b3ddc0d6757ad54188ccae
Mac <mac@Macs-MacBook-Pro.local>
2023-03-04 20:38:09 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,41 @@
+var starBlack = document.querySelector(".star-black");
+var starWhite = document.querySelector(".star-white");
+var starBlack1 = document.querySelector(".star-black1");
+var starWhite1 = document.querySelector(".star-white1");
+var starBlack2 = document.querySelector(".star-black2");
+var starWhite2 = document.querySelector(".star-white2");
+var starBlack3 = document.querySelector(".star-black3");
+var starWhite3 = document.querySelector(".star-white3");
+var starBlack4 = document.querySelector(".star-black4");
+var starWhite4 = document.querySelector(".star-white4");
+var starBlack5 = document.querySelector(".star-black5");
+var starWhite5 = document.querySelector(".star-white5");
+
+function anstarWhite() {
+  starBlack.style.display = "block";
+  starWhite.style.display = "none";
+}
+
+function anstarWhite1() {
+  starBlack1.style.display = "block";
+  starWhite1.style.display = "none";
+}
+function anstarWhite2() {
+  starBlack2.style.display = "block";
+  starWhite2.style.display = "none";
+}
+
+function anstarWhite3() {
+  starBlack3.style.display = "block";
+  starWhite3.style.display = "none";
+}
+
+function anstarWhite4() {
+  starBlack4.style.display = "block";
+  starWhite4.style.display = "none";
+}
+
+function anstarWhite5() {
+  starBlack5.style.display = "block";
+  starWhite5.style.display = "none";
+}
